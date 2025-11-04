9f5a0cb52a76ead90757e97359a53474214fc355
thecoderbee <rishikeshkumar.work@gmail.com>
2025-11-04 09:30:09 +0530

    Add Alembic for database migrations

--- src/gookybot/database/connection.py
+++ src/gookybot/database/connection.py
@@ -15,6 +15,7 @@ engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
+# for manual creation
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
